510bf55d03b3d23edfc182a35b425522862e490d
Matthieu Pinte <matthieupinte@gmail.com>
2017-07-27 12:35:23 +0200

    fix: Use String.to_existing_atom instead of String.to_atom. - by @kdisneur

--- lib/ecto_atom.ex
+++ lib/ecto_atom.ex
@@ -19,7 +19,7 @@ defmodule Ecto.Atom do
   def cast(value) when is_atom(value), do: {:ok, value}
   def cast(_), do: :error
 
-  def load(value), do: {:ok, String.to_atom(value)}
+  def load(value), do: {:ok, String.to_existing_atom(value)}
 
   def dump(value) when is_atom(value), do: {:ok, Atom.to_string(value)}
   def dump(_), do: :error
